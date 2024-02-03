e1e8e4da35a957528ad12abc89f68b3c66751785
Mohammed Ali <mohammed.ali.baaqail@gmail.com>
2024-02-04 00:03:03 +0300

    ..

--- app/page.jsx
+++ app/page.jsx
@@ -8,40 +8,51 @@ const Home = () => {
   const [activeTab, setActiveTab] = useState("textToSpeech");
 
   return (
-    <main className="container p-4 flex flex-col justify-start items-center min-h-screen">
-      <div className="flex justify-between w-full border rounded-t-xl border-gray-200 p-8 bg-[#fcfcfc] max-md:flex-col max-md:p-3">
-        <div><h3>Task</h3></div>
-        <div className="flex justify-around w-10/12 max-md:flex-col max-md:w-full max-md:items-center">
-          <button
-            className={`py-2 px-4 rounded max-md:w-2/3 max-md:my-2 my-auto ${
+    <main className="container my-8  flex flex-col justify-start items-center min-h-screen">
+      <div className="flex justify-between w-full border rounded-t-xl border-gray-200  bg-[#fcfcfc] max-md:flex-col ">
+        {/* <div><h3>Task</h3></div> */}
+        <div className="flex justify-around w-full  max-md:flex-col max-md:w-full max-md:items-center">
+          <div
+            className={`flex  justify-center items-center cursor-pointer py-2 px-4 w-full h-14 rounded-tl  ${
               activeTab === "textToSpeech"
-                ? "bg-gray-900 text-white"
-                : "bg-gray-200 text-gray-700"
+                ? "bg-gray-900 "
+                : "bg-gray-200 "
             }`}
             onClick={() => setActiveTab("textToSpeech")}
           >
-            Text To Speech
-          </button>
-          <button
-            className={`py-2 px-4 rounded max-md:w-2/3 max-md:my-2 my-auto ${
+
+                  <h3 className={`text-center ${activeTab === "textToSpeech"
+                ? " text-white"
+                : " text-gray-700" }`}>Text To Speech</h3>
+            
+          </div>
+          <div
+            className={`flex justify-center items-center cursor-pointer py-2 px-4 w-full h-14  ${
               activeTab === "speechToText"
-                ? "bg-gray-900 text-white"
-                : "bg-gray-200 text-gray-700"
+                ? "bg-gray-900 "
+                : "bg-gray-200 "
             }`}
             onClick={() => setActiveTab("speechToText")}
           >
-            Speech To Text
-          </button>
-          <button
-            className={`py-2 px-4 rounded max-md:w-2/3 max-md:my-2 my-auto ${
+            <h3 className={`text-center ${activeTab === "speechToText"
+                ? " text-white"
+                : " text-gray-700" }`}>Speech To Text</h3>
+            
+          </div>
+          <div
+            className={` flex justify-center items-center cursor-pointer py-2 px-4 w-full h-14 rounded-tr m ${
               activeTab === "TextToVideo"
-                ? "bg-gray-900 text-white"
-                : "bg-gray-200 text-gray-700"
+                ? "bg-gray-900 "
+                : "bg-gray-200 "
             }`}
             onClick={() => setActiveTab("TextToVideo")}
           >
-            Text To Video
-          </button> 
+
+                       <h3 className={`text-center ${activeTab === "TextToVideo"
+                ? " text-white"
+                : " text-gray-700" }`}>Text To Video</h3>
+            
+          </div> 
         </div>
       </div>
 
